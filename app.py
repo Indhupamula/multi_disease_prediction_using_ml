fa75940f00570d567347d6129f357ad11bcf6b81
Pamula Indhu <146244249+Indhupamula@users.noreply.github.com>
2025-10-04 21:21:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 26 2023
-
-@author: AK
-"""
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu 
-
-
 # loading the saved models
 
 diabetes_model = pickle.load(open("./models/diabetes_model_new.sav",'rb'))
@@ -19,15 +11,64 @@ breast_model = pickle.load(open("./models/breast_cancer_model.sav",'rb'))
 # sidebar navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System', 
-                           ['Heart Disease Prediction',
+    selected = option_menu('Multiple Disease Prediction System using ML', 
+                           [
                             'Diabetes Prediction',
+                            'Heart Disease Prediction',
                             'Parkinson\'s Prediction',
                             'Breast Cancer Prediction'],
                            icons=['heart','activity','person','gender-female'],
                            default_index=0)
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
     
-
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+        
+    # code for prediction
+    diab_diagnosis=''
+    
+    # create a button for prediction
+    
+    if st.button('Diabetes Test Result'):
+        if not all([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]):
+            st.warning("Please fill in all the fields.")
+        else:
+            diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+            
+            if (diab_prediction[0] == 1):
+              diab_diagnosis = 'The person is diabetic'
+            else:
+              diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)  
 # Heart Disease Prediction Page   
 if (selected == 'Heart Disease Prediction'): 
     # page title
@@ -93,67 +134,6 @@ if (selected == 'Heart Disease Prediction'):
               heart_diagnosis = 'The person does not have any heart disease'
         
     st.success(heart_diagnosis)  
-
-
-
-
-
-
-# Diabetes Prediction Page
-if (selected == 'Diabetes Prediction'):
-    
-    # page title
-    st.title('Diabetes Prediction using ML')
-    
-    # getting the input data from the user
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
-        
-    with col2:
-        Glucose = st.text_input('Glucose Level')
-    
-    with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
-    
-    with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
-    
-    with col2:
-        Insulin = st.text_input('Insulin Level')
-    
-    with col3:
-        BMI = st.text_input('BMI value')
-    
-    with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    
-    with col2:
-        Age = st.text_input('Age of the Person')
-        
-    # code for prediction
-    diab_diagnosis=''
-    
-    # create a button for prediction
-    
-    if st.button('Diabetes Test Result'):
-        if not all([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]):
-            st.warning("Please fill in all the fields.")
-        else:
-            diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-            
-            if (diab_prediction[0] == 1):
-              diab_diagnosis = 'The person is diabetic'
-            else:
-              diab_diagnosis = 'The person is not diabetic'
-        
-    st.success(diab_diagnosis)  
-  
-
-    
-
-
 # Parkinsons Prediction Page  
 if (selected == 'Parkinson\'s Prediction'):    
     
@@ -359,15 +339,7 @@ if selected == 'Breast Cancer Prediction':
 
     
 
+  
 
-
-
-
-
-    
-    
-    
-    
-    
     
     
